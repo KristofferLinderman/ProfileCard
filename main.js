ca162be932577326ccf238cf97b0d6631e853526
Kristoffer <Kristoffermglinderman@gmail.com>
2019-10-28 21:56:59 +0100

    added animations to icons

--- main.js
+++ main.js
@@ -1,26 +1,58 @@
 const profileCard = document.getElementById('profileCard');
+
 const followCount = document.getElementById('followCount');
 const retweetCount = document.getElementById('retweetCount');
 const likeCount = document.getElementById('likeCount');
 
-window.onload = () => {
-    console.log("Ready");
-}
+const likeIcon = document.getElementById('likeIcon');
+const retweetIcon = document.getElementById('retweetIcon');
+const followIcon = document.getElementById('followIcon');
 
-profileCard.addEventListener('mouseover', () => {
-    initiateCounter(followCount, 150, 6500);
-    initiateCounter(retweetCount, 150, 13400);
-    initiateCounter(likeCount, 150, 7900);
+profileCard.addEventListener('mouseenter', () => {
+    setTimeout(animateNumbers, 250);
 });
 
-function initiateCounter(element, startCount, limitCount) {
-    let incerement = limitCount / startCount * 1000;
-    let interval = setInterval(() => {
-        element.innerText = startCount > 1000 ? Math.round(startCount / 1000) + "." + (Math.floor(Math.random() * 10)) + "K" : startCount;
-        startCount += incerement;
+profileCard.addEventListener('mouseleave', () => {
+    removeColor();
+});
 
-        if (startCount > limitCount) {
+function resetNumbers() {
+    followCount.innerText = "0.0K";
+    retweetCount.innerText = "0.0K";
+    likeCount.innerText = "0.0K";
+}
+
+function animateNumbers() {
+    addColor();
+    initiateCounter(followCount, 16345);
+    initiateCounter(retweetCount, 13400);
+    initiateCounter(likeCount, 7900);
+}
+
+function addColor() {
+    likeIcon.classList.add("pulsate");
+    retweetIcon.classList.add("spin");
+    followIcon.classList.add("wiggle");
+}
+
+function removeColor() {
+    likeIcon.classList.remove("pulsate");
+    retweetIcon.classList.remove("spin");
+    followIcon.classList.remove("wiggle");
+}
+
+function initiateCounter(element, limitCount) {
+    let count = 0;
+    let incerement = 675;
+
+    let interval = setInterval(() => {
+
+        count += incerement;
+
+        element.innerText = count > 1000 ? Math.round(count / 1000) + "." + Math.floor(Math.random() * 10) + "K" : count;
+
+        if (count > limitCount) {
             clearInterval(interval);
         }
-    }, 20);
+    }, 30);
 }
